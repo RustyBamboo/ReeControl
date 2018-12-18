753da0f3c4372c6945041c20cbd770adeeab8ccd
RustyBamboo <dvolya@gmail.com>
2018-12-18 10:21:27 -0500

    fix to use angular velocity vector

--- src/control.rs
+++ src/control.rs
@@ -2,7 +2,7 @@ extern crate serde_yaml;
 use serde_yaml::Value;
 use std::collections::HashMap;
 extern crate nalgebra as na;
-use na::{Vector3, Matrix};
+use na::{Vector3, Matrix, UnitQuaternion};
 
 /* Helper structs for thruster_mapper.yaml
  * Mostly temporary to test code
@@ -43,16 +43,22 @@ pub fn get_b_matrix() -> na::MatrixMN<f64, na::U6, na::U8> {
 
     for name in thruster_order.iter() {
         let value = yaml.thrusters.get::<str>(name).unwrap();
-        println!("Thruster: {}, position: {:#?}, direction: {:#?}, bounds: {:#?}", name, value.position, value.direction, value.thrust_bounds);
 
         let position = Vector3::new(value.position[0], value.position[1], value.position[2]);
         let direction = Vector3::new(value.direction[0], value.direction[1], value.direction[2]);
         
         let torque = position.cross(&direction);
+        println!("Thruster: {}, position: {:#?}, direction: {:#?}, torque: {:#?}, bounds: {:#?}", name, position, direction, torque, value.thrust_bounds);
         let column =  na::Vector6::new(value.direction[0], value.direction[1], value.direction[2], torque[0], torque[1], torque[2]);
  
         cols.push(column);
         bounds.push(&value.thrust_bounds);
     }
     na::MatrixMN::<f64, na::U6, na::U8>::from_columns(&cols)
+}
+
+pub fn pid(current: (Vector3<f64>, UnitQuaternion<f64>), desired: (Vector3<f64>, UnitQuaternion<f64>)) -> () {
+    let (current_position, current_orientation) = current;
+    let (desired_position, desired_orientation) = desired;
+
 }
--- src/main.rs
+++ src/main.rs
@@ -34,47 +34,50 @@ struct Sub {
     velocity: Translation3<f32>,
     acceleration: Translation3<f32>,
     angular_position: UnitQuaternion<f32>,
-    angular_velocity: UnitQuaternion<f32>,
-    angular_acceleration: UnitQuaternion<f32>,
+    angular_velocity: Vector3<f32>,
+    angular_acceleration: Vector3<f32>,
 
     c: SceneNode
 }
 
 impl Sub {
+    // Update object by integrating linear and angular components
     fn update(&mut self, drag:&Drag, a:Vector6<f64>) -> () {
+        // Apply linear acceleration
         let apply_acc = Translation3::new(a[0] as f32, a[1] as f32, a[2] as f32);
-        self.velocity.vector += self.acceleration.vector + apply_acc.vector;
-        self.position.vector += drag.velocity * self.velocity.vector;
+        self.velocity.vector += self.acceleration.vector + self.angular_position.to_rotation_matrix() * apply_acc.vector;
+        self.position.vector += drag.velocity * (self.angular_position.to_rotation_matrix() *self.velocity.vector);
 
-
+        // Apply angular 
         let axis_vec = Vector3::new(a[3] as f32, a[4] as f32, a[5] as f32); 
-        let axis_vec_norm = axis_vec.magnitude();
+        self.angular_acceleration += axis_vec;
 
-        let mut apply_ang_acc = na::UnitQuaternion::identity();
+        // cross product is linear
+        self.angular_velocity += self.angular_acceleration;
+        // Apply angular drag
+        self.angular_velocity *= drag.angular_velocity;
 
-        // Only apply rotation if significant torque
-        if axis_vec_norm.abs() > 0.0001 { 
-            let axis_vec_normed = na::Unit::new_unchecked(axis_vec / axis_vec_norm);
-            apply_ang_acc = na::UnitQuaternion::from_axis_angle(&axis_vec_normed, axis_vec_norm*0.001);
+        // Don't attempt if small rotation
+        if self.angular_velocity.magnitude().abs() > 0.0001 {
+            let mut quat_vel = na::UnitQuaternion::from_axis_angle(&na::Unit::new_unchecked(self.angular_velocity.normalize()), self.angular_velocity.magnitude()); 
+            self.angular_position *= quat_vel;
         }
-        
-        self.angular_velocity *= self.angular_acceleration * apply_ang_acc;
-        self.angular_position *= self.angular_velocity;
 
         //self.c.set_local_rotation(self.angular_position);
         //self.c.append_translation(&self.position);
         let iso = na::Isometry3::from_parts(self.position, self.angular_position);
-        self.c.append_transformation(&iso);
+        self.c.set_local_transformation(iso);
     }
     fn update_camera(&mut self, camera: &mut ArcBall) {
         camera.set_at(Point3::new(self.position.vector.x, self.position.vector.y, self.position.vector.z));
     }
 
+    // Create new cube
     fn new(window:&mut Window) -> Self {
         let mut c = window.add_cube(1.0, 1.0, 1.0);
         c.set_color(1.0, 0.0, 0.0);
         Sub{position:Translation3::new(0.0, 0.0, 0.0), velocity:Translation3::new(0.0, 0.0, 0.0), acceleration:Translation3::new(0.0, 0.0, 0.0),
-            angular_position:UnitQuaternion::identity(), angular_velocity:UnitQuaternion::identity(), angular_acceleration:UnitQuaternion::identity(), c: c} 
+            angular_position:UnitQuaternion::identity(), angular_velocity:Vector3::zeros(), angular_acceleration:Vector3::zeros(), c: c} 
     }
 }
 
@@ -88,12 +91,12 @@ fn main() {
     let mut sub = Sub::new(&mut window);
     sub.acceleration = Translation3::new(0.0001, 0.0, 0.0);
     
-    let drag = Drag {velocity:0.001, angular_velocity:0.01};
+    let drag = Drag {velocity:0.001, angular_velocity:0.001};
 
     let b_matrix = control::get_b_matrix();
     println!("B Matrix: {}", b_matrix);
     let mut thrust :Vector8<f64> = Vector8::<f64>::repeat(0.0);
-    thrust[0] = 0.001; 
+    thrust[1] = 0.1; 
     println!("{}", thrust);
     let out = b_matrix * thrust;
     println!("{}", out);
